c28a9a14a93d45ab8a8b2bb4a1d12e3b4d2cd8cb
Ahmed-Nagah103 <ahmednagah161@gmail.com>
2024-08-19 10:30:04 +0300

    added Home Page Screen , Chats Screen

--- lib/moduls/home/presentation/screens/home_page_screen.dart
+++ lib/moduls/home/presentation/screens/home_page_screen.dart
@@ -1,6 +1,6 @@
 import 'package:chat_app/core/utils/constants/app_constants.dart';
 import 'package:chat_app/core/utils/extensions.dart';
-import 'package:chat_app/moduls/home/presentation/screens/chat_screen.dart';
+import 'package:chat_app/moduls/chat/presentation/screens/chat_screen.dart';
 import 'package:chat_app/moduls/home/presentation/widgets/picture_item.dart';
 import 'package:flutter/material.dart';
 
