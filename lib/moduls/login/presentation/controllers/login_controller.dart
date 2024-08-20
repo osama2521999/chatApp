b73b353e3531609749bcd589e0bc945f2ee747c8
osama.ahmed <osama.ahmed@easwaaqmisr.com>
2024-08-20 13:10:42 +0300

    login_handling

--- lib/moduls/login/presentation/controllers/login_controller.dart
+++ lib/moduls/login/presentation/controllers/login_controller.dart
@@ -1,26 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginController{
-
+class LoginController {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<User?> signInEmail(String email, String password) async {
-    UserCredential result =
-    await auth.signInWithEmailAndPassword(email: email, password: password);
-    final User? user = result.user;
-    user!=null? print('user signed in'):print('failed signing');
-    print(user?.email);
-    return user;
-
+    User? user;
+    try {
+      UserCredential result = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = result.user;
+      user != null ? print('user signed in') : print('failed signing');
+      print(user?.email);
+      return user;
+    } on Exception catch (e) {
+      print("osama${e}");
+      return user;
+    }
   }
+
   Future<bool> signUp(email, password) async {
     UserCredential result = await auth.createUserWithEmailAndPassword(
         email: email, password: password);
 
-
     final User? user = result.user;
-
-
 
     return user != null ? true : false;
   }
--- lib/moduls/login/presentation/screens/login_screen.dart
+++ lib/moduls/login/presentation/screens/login_screen.dart
@@ -3,15 +3,34 @@ import 'package:chat_app/core/utils/extensions.dart';
 import 'package:chat_app/moduls/login/presentation/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-   LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
 
-  final TextEditingController emailController=TextEditingController();
-  final TextEditingController passController=TextEditingController();
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passController = TextEditingController();
+
+  bool checkLogin = false;
+
+  void login() async {
+    setState(() {
+      checkLogin = true;
+    });
+    await LoginController()
+        .signInEmail(emailController.text, passController.text);
+    setState(() {
+      checkLogin = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: AppConstants.mainColor,
       appBar: AppBar(
         automaticallyImplyLeading: true,
@@ -20,67 +39,89 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
-            padding:  EdgeInsetsDirectional.symmetric(horizontal:  AppConstants.appPadding),
+            padding: EdgeInsetsDirectional.symmetric(
+                horizontal: AppConstants.appPadding),
             child: Column(
               children: [
-                Text('Login with your email or phone',style: TextStyle(fontSize: 24,color: Colors.white)),
-                SizedBox(height:  context.height()/30,),
-
+                Text('Login with your email or phone',
+                    style: TextStyle(fontSize: 24, color: Colors.white)),
+                SizedBox(
+                  height: context.height() / 30,
+                ),
                 TextFormField(
-                  controller: emailController  ,
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
+                  controller: emailController,
+                  style: TextStyle(color: Colors.white),
                   decoration: InputDecoration(
                       hintText: 'Email',
                       hintStyle: TextStyle(color: Colors.grey),
-
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       )),
                 ),
-                SizedBox(height:  context.height()/50,),
+                SizedBox(
+                  height: context.height() / 50,
+                ),
                 TextFormField(
-                  controller:passController ,
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
+                  controller: passController,
+                  style: TextStyle(color: Colors.white),
                   decoration: InputDecoration(
                       hintText: 'Password',
                       hintStyle: TextStyle(color: Colors.grey),
-
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       )),
                 ),
-                SizedBox(height:  context.height()/30,),
+                SizedBox(
+                  height: context.height() / 30,
+                ),
+                checkLogin
+                    ? CircularProgressIndicator()
+                    : Container(
+                        width: context.width(),
+                        height: context.height() / 12,
+                        decoration: BoxDecoration(
+                            color: Color(0Xff303030),
+                            borderRadius: BorderRadius.circular(
+                                AppConstants.buttonsBorderRadius)),
+                        child: TextButton(
+                            onPressed: () {
+                              login();
+                            },
+                            child: Text(
+                              'Log in',
+                              style: TextStyle(color: Colors.white),
+                            ))),
+                SizedBox(
+                  height: context.height() / 50,
+                ),
                 Container(
                     width: context.width(),
-                    height:context.height()/12 ,
+                    height: context.height() / 12,
                     decoration: BoxDecoration(
-                      color: Color(0Xff303030),
-                      borderRadius: BorderRadius.circular(AppConstants.buttonsBorderRadius)
-                    ),
-                    child: TextButton(onPressed: ()async{
-
-                      await LoginController().signInEmail(emailController.text, passController.text);
-
-                    }, child: Text('Log in',style:  TextStyle(color: Colors.white),))),
-                SizedBox(height:  context.height()/50,),
-                Container(
-                    width: context.width(),
-                    height:context.height()/12 ,
-                    decoration: BoxDecoration(
-                      color: Color(0Xff303030),
-                      borderRadius: BorderRadius.circular(AppConstants.buttonsBorderRadius)
-                    ),
-                    child: TextButton(onPressed: ()async{
-
-                     await LoginController().signUp(emailController.text, passController.text) ? print('userCreated'):print('failed');
-
-                    }, child: Text('Creat New Account ',style:  TextStyle(color: Colors.white),))),
-                SizedBox(height:  context.height()/30,),
-                TextButton(onPressed: (){}, child: Text('Forget Password?',style: TextStyle(color: Colors.blue),),)
+                        color: Color(0Xff303030),
+                        borderRadius: BorderRadius.circular(
+                            AppConstants.buttonsBorderRadius)),
+                    child: TextButton(
+                        onPressed: () async {
+                          await LoginController().signUp(
+                                  emailController.text, passController.text)
+                              ? print('userCreated')
+                              : print('failed');
+                        },
+                        child: Text(
+                          'Creat New Account ',
+                          style: TextStyle(color: Colors.white),
+                        ))),
+                SizedBox(
+                  height: context.height() / 30,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forget Password?',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                )
               ],
             ),
           ),
