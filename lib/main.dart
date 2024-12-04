8ed633deb997b17b564cd38ab2a6ae4ce7b8dbbf
Ahmed Nagah <Ahmednagah161@gmail.com>
2024-12-04 12:01:09 +0200

    analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/moduls/login/presentation/screens/login_screen.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ void main() async{
   await Firebase.initializeApp(
       options: FirebaseAppOptions.options()
   );
+  FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
   runApp(const MyApp());
 }
 
